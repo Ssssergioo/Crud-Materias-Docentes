1e106e038dc97e1fc08b261a57785236e52bd48f
Ssssergioo <136131327+Ssssergioo@users.noreply.github.com>
2024-05-01 17:07:57 -0400

    Creacion de Crud y Clases Docente y docentes

--- CrudDocentesyMaterias/Docentes/Docentes.js
+++ CrudDocentesyMaterias/Docentes/Docentes.js
@@ -1,2 +1,48 @@
-//Aca va el array donde vamos a guardar a los docentes con sus datos
-//Tambien el constructor y sus metodos
+class Docentes{
+    datos=[];
+
+    constructor(){
+        let data=localStorage.getItem("Docentes");
+        if(!data){
+            this.predata();
+        }else{
+            this.getdata();
+        }
+        this.persistir();
+    }
+
+getMaxId(){};
+
+predata(){
+    this.datos.push({
+        doce_id: 1,
+        doce_apellido: "Perez",
+        doce_nombre: "Sergio",
+        doce_mail: "ajjaja@gmail.com",
+        doce_cumple: "03/10/2022" ,
+        doce_cell: "0975757757",
+    });
+    this.datos.push({
+        doce_id: 1,
+        doce_apellido: "Ferrandez",
+        doce_nombre: "Arami",
+        doce_mail: "siiii@gmail.com",
+        doce_cumple: "05/11/2024" ,
+        doce_cell: "0975757757",
+    });
+};
+
+getdata(){};
+
+getIndexById(){};
+
+agregar(){};
+
+actualizar(){};
+
+borrar(){};
+
+persistir(){
+    localStorage.setItem("Docentes", JSON.stringify(this.datos));}
+
+}
--- CrudDocentesyMaterias/Docentes/app.js
+++ CrudDocentesyMaterias/Docentes/app.js
@@ -1,1 +1,63 @@
-//Aca donde a hacer todas la funciones, dibujar la tabla y toda esa pesca
+//Funciones
+function nuevoForm(){
+    document.getElementById("doce_id").value= -1;
+    document.getElementById("doce_apellido").value= "";
+    document.getElementById("doce_nombre").value= "";
+    document.getElementById("doce_mail").value= "";
+    document.getElementById("doce_cumple").value= "";
+    document.getElementById("doce_cell").value= "";
+
+};
+function editarForm(e){};
+function borrarForm(e){};
+function guardarForm(){
+    let docente = new Docente();
+    docente.doce_id = document.getElementById("doce_id").value;
+    docente.doce_apellido = document.getElementById("doce_apellido").value= "";
+    docente.doce_nombre = document.getElementById("doce_nombre").value= "";
+    docente.doce_mail = document.getElementById("doce_mail").value= "";
+    docente.doce_cumple = document.getElementById("doce_cumple").value= "";
+    docente.doce_cell = document.getElementById("doce_cell").value= "";
+
+    if(docente.doce_id == -1){
+        docentes.agregar(docente);
+    }else{
+        docentes.actualizar(docente);
+    }
+    dibujarTabla();
+};
+function cancelarForm(){};
+function dibujarTabla(){
+    let lineas = "";
+	let thead =
+		"<table class='table'><thead><tr><th>Apellido</th><th>Nombre</th><th>Mail</th><th>Cumple</th><th>Celular</th><th colspan='2'><a id='btnuevo'  class='btn btn-primary'>Nuevo</a></th></tr> </thead>";
+	let tbody = "<tbody>";
+	let tfoot = "</tbody></table>";
+    docentes.datos.forEach((f) => {
+		lineas =
+			lineas +
+			"<tr><td>" +
+			docentes.getNameById(f.doce_id) +
+			"</td><td>" +
+			f.doce_apellido +
+			"</td><td>" +
+			f.doce_nombre+
+            "</td><td>" +
+			f.doce_mail+
+            "</td><td>" +
+			f.doce_cumple+
+            "</td><td>" +
+			f.doce_cell+
+			" </td> <td><a class='bteditar' data-id='" +
+			f.doce_id +
+			"'>Editar</a>" +
+			"<td><a class='btborrar' data-id='" +
+			f.doce_id +
+			"'>Borrar</a></td></tr>";
+	});
+    document.getElementById("tablaDocente").innerHTML = thead + tbody + lineas + tfoot;
+};
+function asignarEventos(){};
+function cargar(){};
+
+document.addEventListener("DOMContentLoaded", dibujarTabla);
